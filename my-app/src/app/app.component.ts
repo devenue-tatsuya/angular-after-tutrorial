f38594b68988ffd0984032cd03c20402ed3dce87
dosa <tatsuya.sugawara@devenues.com>
2023-07-03 09:13:31 +0900

    状態管理の責任を分割

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AppUsecase } from './app.usecase';
 import { UserListComponent } from './user-list/user-list.component';
 import { User } from './user';
 import { UserService } from './services/user.service';
@@ -9,15 +10,20 @@ import { UserService } from './services/user.service';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, UserListComponent],
+  providers: [AppUsecase],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   private readonly userService = inject(UserService);
+  private readonly usecase = inject(AppUsecase);
+  readonly state$ = this.usecase.state$;
 
   users: User[] = [];
 
   ngOnInit() {
+    this.usecase.initialize();
+
     this.userService.getUsers().subscribe(users => {
       this.users = users;
     });
